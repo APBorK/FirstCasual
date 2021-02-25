77141789eab45d343cfffffcf1a9d462d45685af
APBorK <1659732@gmail.com>
2021-02-25 19:38:23 +0300

    Create SampleScene.meta

---
+++ Assets/Script/MoveSphere.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+
+public class MoveSphere : MonoBehaviour
+{
+    public int speed = 3;
+
+    private Vector3 _lastPos;
+
+    private void Start()
+    {
+        _lastPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+           
+        }
+        
+        _lastPos = transform.position;
+        
+    }
+}
